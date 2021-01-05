9390dd03bb3c1d6bd277703177983ad756a917b4
Heath O'Connell <hoc3426@gmail.com>
2021-01-05 16:48:31 -0600

    fermilab_eprint_report: Check for pre-existing Fermilab links

--- fermilab_eprint_report.py
+++ fermilab_eprint_report.py
@@ -7,10 +7,38 @@ def main(reports):
     """Print the Fermilab report number on a PDF stored at INSPIRE"""
 
     for report in reports:
-        result = get_result('find r ' + report, 'arxiv_eprints,documents')
-        eprint = result[0]['metadata']['arxiv_eprints'][0]['value']
-        url = result[0]['metadata']['documents'][0]['url']
-        print(report, eprint, url)
+        done = False
+        fields = 'arxiv_eprints,documents,urls'
+        result = get_result('find r ' + report, fields)
+        #print(result)
+        try:
+            urls = result[0]['metadata']['urls']
+        except (IndexError, KeyError):
+            continue
+        for url in urls:
+            try:
+                url_desc = url['description']
+            except KeyError:
+                print('Problem with:', report)
+                print(url)
+                quit()
+            if url_desc.lower().startswith('fermilab library'):
+                print(report, 'DONE')
+                done = True
+                break
+        if done:
+            continue
+        try:
+            eprint = result[0]['metadata']['arxiv_eprints'][0]['value']
+            print(report, eprint)
+            continue
+        except (IndexError, KeyError):
+            pass
+        try:
+            url = result[0]['metadata']['documents'][0]['url']
+            print(report, url)
+        except (IndexError, KeyError):
+            continue
 
 if __name__ == '__main__':
     try:
