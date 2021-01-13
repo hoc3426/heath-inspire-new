ed51005ee7c0898ec7b83b496271b57832468546
Heath O'Connell <hoc3426@gmail.com>
2021-01-13 13:31:01 -0600

    check_url: check for accepted PDFs

--- check_url.py
+++ check_url.py
@@ -1,28 +1,66 @@
-'''Checks to see if a URL returns well-formed PDF'''
+'''Checks to see if a URL returns well-formed PDF and accepted manuscripts'''
 
 import PyPDF2
 import requests
 
+TMP_PDF_FILE = '/tmp/file.pdf'
+ACCEPTED_DESC = ['article from scoap3',
+                 'fermilab accepted manuscript',
+                 'fulltext from publisher',
+                 'open access fulltext']
+FERMILAB_DESC = 'fermilab library server'
+
 def check_url(url):
-    '''Checks to see if a URL returns well-formed PDF'''
+    '''Checks a URL to return a well-formed PDF or returns None'''
 
     try:
         response = requests.get(url)
     except requests.exceptions.RequestException as err:
-        print(f'url: {url} could not be reached')
+        print(f'url: {url} could not be reached: {err}')
         return None
     if response.status_code != 200:
-        print(f'url: {url} is not valid')
+        print(f'url: {url} does not work')
         return None
     if response.headers['content-type'] != 'application/pdf':
         print(f'url: {url} does not return a pdf file')
         return None
-    tmp_pdf_file = '/tmp/file.pdf'
-    with open(tmp_pdf_file, 'wb') as fhandle:
+    with open(TMP_PDF_FILE, 'wb') as fhandle:
         fhandle.write(response.content)
     try:
-        PyPDF2.PdfFileReader(open(tmp_pdf_file, 'rb'))
-        return True
+        PyPDF2.PdfFileReader(open(TMP_PDF_FILE, 'rb'))
+        return TMP_PDF_FILE
     except (PyPDF2.utils.PdfReadError, TypeError):
         print(f'url: {url} does not return a valid pdf file')
         return None
+
+def check_accepted(jrec):
+    '''See if a PDF is accepted'''
+
+    accepted = False
+    url = None
+    urls = []
+    try:
+        urls += jrec['metadata']['urls']
+    except KeyError:
+        pass
+    try:
+        urls += jrec['metadata']['documents']
+    except KeyError:
+        pass
+    for url_dict in urls:
+        try:
+            description = url_dict['description'].lower()
+        except KeyError:
+            continue
+        try:
+            url_value = url_dict['value']
+        except KeyError:
+            try:
+                url_value = url_dict['url']
+            except KeyError:
+                continue
+        if description in ACCEPTED_DESC:
+            return [url_value, True]
+        if description.startswith(FERMILAB_DESC):
+            url = url_value
+    return [url, accepted]
