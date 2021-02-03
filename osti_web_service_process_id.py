107d47176b09505c6f364c6fce15429d40ae917a
Heath O'Connell <hoc3426@gmail.com>
2021-02-03 10:13:12 -0600

    osti_web_service_process_id: minor changes

---
+++ osti_web_service_process_id.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+"""
+Script for adding OSTI IDs to INSPIRE records after using OSTI Web Service.
+"""
+
+import re
+import xml.etree.ElementTree as ET
+
+from inspire_api import get_result_ids
+from osti_web_service import create_osti_id_pdf
+from osti_fermilab_accepted_report import get_fermilab_report
+
+TEST = False
+#TEST = True
+DOCUMENT = 'tmp_osti.out'
+VERBOSE = False
+VERBOSE = True
+
+if TEST:
+    DOCUMENT = 'tmp_osti_test.out'
+
+RECIDS = []
+
+
+def check_for_accepted_pdf(osti_id, recid):
+    '''Look at the record to see if it has an accepted PDF to upload'''
+
+    
+
+def print_rec(osti_id, recid):
+    '''Create an xml record to upload'''
+
+    return f'''<record>
+  <controlfield tag="001">{recid}</controlfield>
+  <datafield tag="035" ind1=" " ind2=" ">
+    <subfield code="a">{osti_id}</subfield>
+    <subfield code="9">OSTI</subfield>
+  </datafield>
+</record>'''
+
+def create_xml(osti_id, recid):
+    """
+    The function checks if the OSTI ID should be added to INSPIRE.
+    If so, it builds up that information.
+    """
+
+    osti_id = str(osti_id)
+    recid = str(recid)
+    recid = recid.replace('oai:inspirehep.net:', '')
+    search = f'_collections:Fermilab recid:{recid}'
+    result_recid = get_result_ids(search)
+    if len(result_recid) != 1:
+        print(f'No such INSPIRE Fermilab record {recid}')
+        return None
+    create_osti_id_pdf(recid=recid, osti_id=osti_id)
+    search = '_collections:Fermilab '
+    search += f'external_system_identifiers.value:{osti_id} '
+    search += 'external_system_identifiers.schema:osti'
+    result_osti = get_result_ids(search)
+    if result_osti == result_recid:
+        return None
+    if len(result_osti) == 1:
+        print(f'OSTI ID {osti_id} already on {result_osti[0]}')
+        return None
+    search = f'recid:{recid} -external_system_identifiers.schema:osti'
+    if TEST:
+        print(search)
+    result = get_result_ids(search)
+    if len(result) != 1:
+        print(f'Problem with {recid} {osti_id}')
+        print(f'  {search} {result}')
+        return False
+    if TEST:
+        print(result)
+    return print_rec(osti_id, recid)
+
+
+def main():
+    """
+    Takes the output from an OSTI web push and appends the
+    OSTI IDs to the INSPIRE records.
+    """
+
+    filename = 'tmp_' + __file__
+    filename = re.sub('.py', '_append.out', filename)
+    output = open(filename, 'w')
+    output.write('<collection>')
+    tree = ET.parse(DOCUMENT)
+    root = tree.getroot()
+    for record in root.findall('record'):
+        print(record.tag)
+        osti_id = record.find('osti_id').text
+        if VERBOSE:
+            print(osti_id)
+        if osti_id == '0':
+            continue
+        recid = record.find('other_identifying_nos').text
+        if VERBOSE:
+            print(recid)
+        record_update = create_xml(osti_id, recid)
+        if record_update:
+            try:
+                if TEST:
+                    print(record_update)
+                else:
+                    output.write(record_update)
+            except IOError:
+                print(f'CANNOT print record {record.attrib}')
+    output.write('</collection>')
+    output.close()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Exiting')
